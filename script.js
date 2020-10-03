7c069d43dcc36f83c24b46c4254937ea8f676d4d
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2020-10-03 14:05:09 +0100

    Create html, basic styles and basic js

---
+++ script.js
@@ -0,0 +1,8 @@
+const mainElement = document.querySelector('#main');
+const sections = document.querySelectorAll('.section');
+
+
+document.addEventListener('mousewheel', (event) => {
+    console.log(event.wheelDelta);
+
+})
