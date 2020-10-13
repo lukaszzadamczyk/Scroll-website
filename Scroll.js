1bbf0ec32d816bf80cf2f2149c30fb51891ee4f3
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2020-10-13 19:40:50 +0100

    Add isScrolledIntoView function

--- Scroll.js
+++ Scroll.js
@@ -2,10 +2,25 @@ class Scroll {
   constructor(main) {
     const mainElement = document.querySelector(main);
     this.sections = document.querySelectorAll(".section");
-    this.sectionIndex = 0;
+    const sectionsArr = [...this.sections];
+    const sectionIndex = sectionsArr.findIndex((section) =>
+      this.isScrolledIntoView(section)
+    );
+    console.log(sectionIndex);
+    this.sectionIndex = sectionIndex < 0 ? 0 : sectionIndex;
+
     this.isScroll = false;
   }
 
+  isScrolledIntoView = (element) => {
+    const rect = element.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
+
+    const vissible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+    return vissible;
+  };
   listenScroll = (event) => {
     if (this.isScroll) return;
     this.isScroll = true;
