6b1645bc77f06a966ccfa3f157543c8eade26058
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2020-10-15 12:59:46 +0100

    Add support for keyboard

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const scroller = new Scroll("#main");
 
 document.addEventListener("wheel", (event) => scroller.listenScroll(event));
-document.addEventListener('swipeUp', ()=>scroller.scroll(1));
-document.addEventListener('swipeDown', ()=>scroller.scroll(-1));
+document.addEventListener("swipeUp", () => scroller.scroll(1));
+document.addEventListener("swipeDown", () => scroller.scroll(-1));
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 40) {
+    return scroller.scroll(1);
+  } else if (event.keyCode === 38) {
+    return scroller.scroll(-1);
+  } else {
+    return;
+  }
+});
